f437cd2d91099482684fed00e889aa321077d53e
manoj7019 <124152401+manoj7019@users.noreply.github.com>
2025-02-21 11:22:11 +0530

    first draft of the projects map

---
+++ script.js
@@ -0,0 +1,138 @@
+document.getElementById('INMP').addEventListener('click', function() {
+    document.getElementById('project-card-container-mp').classList.add('project-card-containers');
+    document.getElementById('project-card-container-up').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-hr').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ap').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ct').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-rj').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-gj').classList.remove('project-card-containers');
+})
+document.getElementById('INMP').addEventListener('click', function() {
+    document.getElementById('INMP').style.fill = '#4dff56';
+    document.getElementById('INUP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INHR').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INAP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INCT').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INRJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INGJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+})
+
+document.getElementById('INUP').addEventListener('click', function() {
+    document.getElementById('project-card-container-up').classList.add('project-card-containers');
+    document.getElementById('project-card-container-mp').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-hr').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ap').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ct').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-rj').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-gj').classList.remove('project-card-containers');
+})
+document.getElementById('INUP').addEventListener('click', function() {
+    document.getElementById('INUP').style.fill = '#4dff56';
+    document.getElementById('INMP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INHR').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INAP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INCT').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INRJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INGJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+})
+
+document.getElementById('INHR').addEventListener('click', function() {
+    document.getElementById('project-card-container-hr').classList.add('project-card-containers');
+    document.getElementById('project-card-container-mp').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-up').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ap').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ct').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-rj').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-gj').classList.remove('project-card-containers');
+})
+document.getElementById('INHR').addEventListener('click', function() {
+    document.getElementById('INHR').style.fill = '#4dff56';
+    document.getElementById('INMP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INUP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INAP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INCT').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INRJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INGJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+})
+
+document.getElementById('INAP').addEventListener('click', function() {
+    document.getElementById('project-card-container-ap').classList.add('project-card-containers');
+    document.getElementById('project-card-container-mp').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-up').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-hr').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ct').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-rj').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-gj').classList.remove('project-card-containers');
+})
+document.getElementById('INAP').addEventListener('click', function() {
+    document.getElementById('INAP').style.fill = '#4dff56';
+    document.getElementById('INMP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INUP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INHR').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INCT').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INRJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INGJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+})
+
+document.getElementById('INCT').addEventListener('click', function() {
+    document.getElementById('project-card-container-ct').classList.add('project-card-containers');
+    document.getElementById('project-card-container-mp').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-up').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-hr').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ap').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-rj').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-gj').classList.remove('project-card-containers');
+})
+document.getElementById('INCT').addEventListener('click', function() {
+    document.getElementById('INCT').style.fill = '#4dff56';
+    document.getElementById('INMP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INUP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INHR').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INAP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INRJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INGJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+})
+
+document.getElementById('INRJ').addEventListener('click', function() {
+    document.getElementById('project-card-container-rj').classList.add('project-card-containers');
+    document.getElementById('project-card-container-mp').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-up').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-hr').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ap').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ct').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-gj').classList.remove('project-card-containers');
+})
+document.getElementById('INRJ').addEventListener('click', function() {
+    document.getElementById('INRJ').style.fill = '#4dff56';
+    document.getElementById('INMP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INUP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INHR').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INAP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INCT').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INGJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+})
+
+document.getElementById('INGJ').addEventListener('click', function() {
+    document.getElementById('project-card-container-gj').classList.add('project-card-containers');
+    document.getElementById('project-card-container-mp').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-up').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-hr').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ap').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-ct').classList.remove('project-card-containers');
+    document.getElementById('project-card-container-rj').classList.remove('project-card-containers');
+})
+document.getElementById('INGJ').addEventListener('click', function() {
+    document.getElementById('INGJ').style.fill = '#4dff56';
+    document.getElementById('INMP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INUP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INHR').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INAP').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INCT').style.fill = 'rgba(0, 128, 0, 0.904)';
+    document.getElementById('INRJ').style.fill = 'rgba(0, 128, 0, 0.904)';
+})
+
+
+
+// document.getElementById('INMP').addEventListener('onmouseout', function() {
+//     document.getElementById('project-card-container').style.display = 'none';
+// })
